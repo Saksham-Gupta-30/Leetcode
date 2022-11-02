67a56dad9e01dae25ca2415b642a0f8ca564b5aa
Saksham Gupta <skshmgupt@gmail.com>
2022-11-02 23:33:20 +0530

    Update

---
+++ 377. Combination Sum IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(vector<int>& nums, int target, vector<int>& dp) {
+        if (target == 0) return 1;
+        if (dp[target] != -1) return dp[target];
+        int pick = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (target - nums[i] >= 0) pick += helper(nums, target - nums[i], dp);
+        }
+        return dp[target] = pick;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, -1);
+        return helper(nums, target, dp);
+    }
+};

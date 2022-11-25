afd30ce55e95936dde19276bd3698b8fa26368b5
Saksham Gupta <skshmgupt@gmail.com>
2022-11-25 23:29:41 +0530

    Update

---
+++ 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t = s;
+        int n = s.size();
+        reverse(t.begin(), t.end());
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1]) curr[j] = 1 + prev[j - 1];
+                else curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+        return n - curr[n];
+    }
+};

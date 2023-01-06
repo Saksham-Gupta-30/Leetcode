adad9e3bf323ef901f83696889cedff27a437983
Saksham Gupta <skshmgupt@gmail.com>
2023-01-06 23:34:19 +0530

    Update

---
+++ 1833. Maximum Ice Cream Bars.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        for (auto it: costs) {
+            if (coins >= it) count++, coins -= it;
+            else break;
+        }
+        return count;
+    }
+};
